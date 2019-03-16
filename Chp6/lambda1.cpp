10e2972cf214ef6954c7e7f3f355f076bc569999
Mateo <mateusz.pawlicki@hotmail.com>
2019-03-16 18:58:02 +0100

    implementacja lambda r31

--- Chp6/lambda1.cpp
+++ Chp6/lambda1.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <functional>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -30,8 +31,44 @@ void addDivisorFilter()
     auto divisor = computeDivisor(calc1, calc2);
 
     filters.emplace_back(
-        [&](int value){return value % divisor == 0;}
+        [&divisor](int value){return value % divisor == 0;}
     );
+    /*
+    Gdy tylko wychodzimy z funkcji addDivisor zmienna divosor przestaje istnieć a wywołując funkcję
+    z kontenera prowadzimy do niezdefiniowanego zachowania.
+    C++11 lepiej jawnie określać listę zmiennych lokalnych i parametrów, od których zależy wyrażenie lambda
+    rozwiązanie przekazanie przez wartość [=] ale też może spowodować zawiśnięcie w przypadku kopiowania wskaźników
+    -gdy przez wartoś przekazujemy wskaźnik który jest kopiowany...
+    */
+}
+
+template <typename C>
+void workWithContainer(const C& container)
+{
+    auto calc1 = computeValue1();
+    auto calc2 = computeValue2();
+
+    auto divisor = computeDivisor(calc1, calc2);
+
+    using ContElemT = typename C::value_type;
+
+    using std::begin;
+    using std::end;
+
+    //w C++14 mozemy pozbyć się ContElemT i zastąpić to auto
+    //usuwamy też using ContElemT
+    if(std::all_of(
+        begin(container), end(container),
+        [&](const ContElemT& value)
+        {return value % divisor == 0;}
+    ))
+    {
+        cout <<"its OK! \n";
+    }
+    else
+    {
+        cout <<"its NOK! \n";
+    }
 }
 
 int main()
@@ -41,6 +78,9 @@ int main()
     {
         cout << f(9) <<'\n';
     }
-    
+
+    vector<int> values{3, 6, 9, 12, 15, 2};
+    workWithContainer(values);
+
     return 0;
 }
